1ee191a62500d99abb40d4a2b4f9d2cc67198b24
MadalynJ06 <m-jordan.8@onu.edu>
2025-04-01 12:21:14 -0400

    HealthPool Created

--- PerilsAlongThePlatte/src/perilsalongtheplatte/HealthPool.java
+++ PerilsAlongThePlatte/src/perilsalongtheplatte/HealthPool.java
@@ -1,5 +1,100 @@
 package perilsalongtheplatte;
 
+import java.util.ArrayList;
+
+import perilsalongtheplatte.Perils;
+
 public class HealthPool {
+	Perils peril;
+	int numOfPeople = 5;
+    ArrayList<Integer> personHealth;
+    ArrayList<String> personName;
+    ArrayList<String> whoIsDead;
+    ArrayList<Boolean> isSick; 
+    Boolean dayOfRest;
+	
+    
+	/**
+	 * Initializes the health for any and all pioneers
+	 * @param numOfPeople is used to see how many health integers are needed
+	 */
+	public void initializeHealth(int numOfPeople) {
+		peril = new Perils();
+		personHealth = new ArrayList<>();
+    	for (int i = 0; i < numOfPeople; i++) {
+	    	personHealth.add(100);
+    	}
+	}
+	
+	public String healthPoolCondition() {
+		int healthPool = 0;
+		for (int i = 0; i < numOfPeople; i++) {
+			healthPool = healthPool + personHealth.get(i);
+		}
+		double avgHealth = healthPool / personHealth.size();
+		if (avgHealth > 75) {return "good";}
+		else if (avgHealth > 50) {return "fair";}
+		else if (avgHealth > 25) {return "poor";}
+		else if (avgHealth > 0) {return "terrible";}
+		return "";
+	}
+	
+	/**
+	 * Checks to see if the pioneer is traveling and loses health
+	 */
+	public void healthDecrease(Boolean dayOfRest) {
+		int conditionOfTrail = 1;
+		if(!dayOfRest) {
+			for (int i = 0; i < numOfPeople; i++) {
+				
+				personHealth.set(i, personHealth.get(i) - conditionOfTrail);
+			}
+		}
+	}
+	
+	/**
+	 * Checks to see if the pioneer is resting and gains health
+	 */
+	public void healthIncrease(Boolean dayOfRest) { 
+		if(dayOfRest) {
+			for (int i = 0; i < numOfPeople; i++) {
+				personHealth.set(i, personHealth.get(i) + 5);
+			}
+		}
+	}
+	
+	/**
+	 * Checks if the pioneer is sick and needs to lose additional health
+	 * @return true or false depending on if the pioneer is sick
+	 */
+	public void checkIfSick() {
+		for(int i = 0; i < numOfPeople; i++) {
+			if (peril.getsSick(personHealth.get(i)) == true) {
+				isSick.set(i, true);
+				personHealth.set(i, personHealth.get(i) - peril.sicknessObtained(isSick.get(i)));
+			}
+		}
+	}
+	
+	/**
+	 * Checks if the pioneer is dead
+	 * @param health is checked to see if it is equal to or below 0
+	 * @return true or false depending on if the pioneer has died
+	 */
+	public void checkIfDead() {
+		for(int i = 0; i < numOfPeople; i++) {
+			int health = personHealth.get(i);
+			if( health <= 0) {
+				System.out.println(personName.get(i) + " has died.");
+				whoIsDead.add(personName.get(i));
+				personHealth.remove(i);
+				personName.remove(i);
+				isSick.remove(i);
+				numOfPeople =- 1;
+			}
+		}
+	}
+	
+
 
 }
