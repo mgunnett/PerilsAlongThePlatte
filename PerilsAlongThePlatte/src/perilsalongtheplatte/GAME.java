445c817b56f7e1b8b1a1204adaa8df87ae8ba724
mgunnett <m-gunnett@onu.edu>
2025-04-01 12:39:33 -0400

    Made new GAME file

--- PerilsAlongThePlatte/src/perilsalongtheplatte/GAME.java
+++ PerilsAlongThePlatte/src/perilsalongtheplatte/GAME.java
@@ -4,7 +4,7 @@ import java.awt.EventQueue;
 
 import javax.swing.JFrame;
 
-public class GAME {
+public class GAME { 
 
 	private JFrame frame;
 
--- PerilsAlongThePlatte/src/perilsalongtheplatte/GUI.java
+++ PerilsAlongThePlatte/src/perilsalongtheplatte/GUI.java
@@ -1,5 +1,27 @@
+/**
+ * Contains and controls the GUI displays and controls in the Perils Along the Platte game. 
+ * */
+
 package perilsalongtheplatte;
 
 public class GUI {
 
+	public int Backgrounds() {
+		return 0;
+	}
+	
+	public int Cutscenes() {
+		return 0;
+	}
+	
+	public int OptionScreen() {
+		return 0;
+	}
+	
+	public int PlayerOptions() {
+		return 0;
+	}
+	
 }
+
+
