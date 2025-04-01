8d3e9b1973cd842f1c3f8673b215d90d3b9bce7c
BoldOneKenobi <westp0921@gmail.com>
2025-04-01 10:42:35 -0400

    Starting work on DailyEvents class

--- PerilsAlongThePlatte/src/perilsalongtheplatte/DailyEvents.java
+++ PerilsAlongThePlatte/src/perilsalongtheplatte/DailyEvents.java
@@ -5,6 +5,37 @@ package perilsalongtheplatte;
  * These events have a set probability of happening, and multiple events can happen per day. Some events have a 
  * different chance on happening based on the gender of the player. 
  */
-public class DailyEvents {
+
+public class DailyEvents{
+	//create an array to store the name of each weather event
+	String weather[] = {"Sun", "Rain", "Thunderstorm", "Snow", "Wind"}; 
+	
+	public boolean hasWeather() {
+		if ()
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	/**
+	 * A method that logs all events that happen to the player in a nice, convenient box in the application window. 
+	 * This method calls other methods in this function to log results. 
+	 */
+	public void eventLog() {
+		;
+	}
 
 }
