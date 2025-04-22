d113d6ba2996ebcae599e4d4ae8c80432aeb8c8e
MadalynJ06 <m-jordan.8@onu.edu>
2025-04-22 15:20:47 -0400

    Dedicating variables for the educational information

--- PerilsAlongThePlatte/src/perilsalongtheplatte/LandmarkDescriptions.java
+++ PerilsAlongThePlatte/src/perilsalongtheplatte/LandmarkDescriptions.java
@@ -1,5 +1,24 @@
 package perilsalongtheplatte;
 
 public class LandmarkDescriptions {
+	public String kansasRiver = "";
+	
+	public String bigBlueRiver = "";
+	
+	public String fortKearny = "";
+
+	public String chimneyRock = "";
+	
+	public String fortLaramie = "";
+	
+	public String huntingInfo = "";
+	
+	public String tradingInfo = "";
+	
+	public String illnessInfo = "";
+	
+	public String beginningInfo = "";
+	
+	public String endingInfo = "";
 
 }
