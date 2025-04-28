17b1ee348473d3c72f4e186fb7ee33128f6d3b04
MadalynJ06 <m-jordan.8@onu.edu>
2025-04-28 16:51:54 -0400

    Edited Educational Descriptions

--- PerilsAlongThePlatte/src/perilsalongtheplatte/LandmarkDescriptions.java
+++ PerilsAlongThePlatte/src/perilsalongtheplatte/LandmarkDescriptions.java
@@ -1,12 +1,16 @@
 package perilsalongtheplatte;
 
+import javax.swing.JOptionPane;
+
 public class LandmarkDescriptions {
-	public String kansasRiver = "The Kansas River begins in Kansas City and travels"
+	
+	// JOptionPane.showMessageDialog(null, kansasRiver, "Kansas River Information:", JOptionPane.INFORMATION_MESSAGE);
+	public String kansasRiver = "     The Kansas River begins in Kansas City and travels"
 			+ "\nwestward to Junction City. This river stretches for"
 			+ "\n170 miles and was one of the first major landmarks"
 			+ "\nthat pioneers encountered after leaving Independence."
 			+ "\nIt offered important resources for settlers, but it"
-			+ "\nalso posed a significant challenge for those attempting"
+			+ " also \nposed a significant challenge for those attempting"
 			+ "\nto cross it.";
 	/**
 	 *  The Kansas River begins in Kansas City and travels
@@ -18,7 +22,8 @@ public class LandmarkDescriptions {
 	 *  to cross it.
 	 */
 	
-	public String bigBlueRiver = "The Big Blue River is one of the rivers that flows into"
+	// JOptionPane.showMessageDialog(null, bigBlueRiver, "Big Blue River Information:", JOptionPane.INFORMATION_MESSAGE);
+	public String bigBlueRiver = "     The Big Blue River is one of the rivers that flows into"
 			+ "\nthe Kansas River. Stretching 359 miles, it runs from Kansas"
 			+ "\nto central Nebraska. This river posed a significant challenge"
 			+ "\nfor travelers on the Oregon Trail, often forcing them to wait"
@@ -33,8 +38,9 @@ public class LandmarkDescriptions {
 	 * was dangerous.	
 	 */
 	
-	public String fortKearny = "Fort Kearny was constructed in 1848 to protect travelers on"
-			+ "\ntheir journey. It also served as a base for \"Pony Riders,\""
+	// JOptionPane.showMessageDialog(null, fortKearny, "Fort Kearny Information:", JOptionPane.INFORMATION_MESSAGE);
+	public String fortKearny = "     Fort Kearny was constructed in 1848 to protect travelers \non"
+			+ " their journey. It also served as a base for \"Pony Riders,\""
 			+ "\nwho were men tasked with delivering messages along the trail."
 			+ "\nThe fort was dismantled after 1871 and now the area is a"
 			+ "\nstate historical park";
@@ -46,7 +52,8 @@ public class LandmarkDescriptions {
 	 * state historical park
 	 */
 
-	public String chimneyRock = "Chimney Rock is a natural formation located along a trail"
+	// JOptionPane.showMessageDialog(null, chimneyRock, "Chimney Rock Information:", JOptionPane.INFORMATION_MESSAGE);
+	public String chimneyRock = "     Chimney Rock is a natural formation located along a trail"
 			+ "\nleading up from the base of a hill. This landmark became a"
 			+ "\nsymbol of progress for pioneers journeying along the Oregon"
 			+ "\nTrail. Many travelers admired its beauty, and it became one"
@@ -61,10 +68,11 @@ public class LandmarkDescriptions {
 	 * that time.
 	 */
 	
-	public String fortLaramie = "Fort Laramie served as an important stop for both Native Americans"
-			+ "\nand those traveling westward. It began as a fur trading post,"
-			+ "\ntransitioned into a haven for emigrants, and eventually became a"
-			+ "\nU.S. Army fort. Over time, it grew into a vital and well-known"
+	// JOptionPane.showMessageDialog(null, fortLaramie, "Fort Laramie Information:", JOptionPane.INFORMATION_MESSAGE);
+	public String fortLaramie = "     Fort Laramie served as an important stop for both Native \nAmericans"
+			+ " and those traveling westward. It began as a fur trading \npost,"
+			+ " transitioned into a haven for emigrants, and eventually became \na"
+			+ " U.S. Army fort. Over time, it grew into a vital and well-known"
 			+ "\nlocation for trading and resting along the trail.";
 	/**
 	 * Fort Laramie served as an important stop for both Native Americans
@@ -74,13 +82,14 @@ public class LandmarkDescriptions {
 	 * location for trading and resting along the trail.
 	 */
 	
-	public String huntingInfo = "Hunting played a crucial role on the Oregon Trail since it was"
+	// JOptionPane.showMessageDialog(null, huntingInfo, "Hunting Information:", JOptionPane.INFORMATION_MESSAGE);
+	public String huntingInfo = "     Hunting played a crucial role on the Oregon Trail since it was"
 			+ "\none of the few ways for travelers to secure additional food for"
 			+ "\ntheir group. In addition to providing meals, hunting could yield"
 			+ "\nvaluable pelts that pioneers might sell or trade for essential"
 			+ "\nsupplies. However, the success of a hunting trip depended on"
-			+ "\nseveral factors, such as whether the group had enough ammunition"
-			+ "\nor energy to hunt and enough space to store their catch.";
+			+ "\nseveral factors, such as whether the group had enough \nammunition"
+			+ " or energy to hunt and enough space to store their \ncatch.";
 	/**
 	 * Hunting played a crucial role on the Oregon Trail since it was
 	 * one of the few ways for travelers to secure additional food for
@@ -91,12 +100,13 @@ public class LandmarkDescriptions {
 	 * or energy to hunt and enough space to store their catch.
 	 */
 	
-	public String tradingInfo = "Trading played a significant role in the Oregon Trail experience."
+	// JOptionPane.showMessageDialog(null, tradingInfo, "Trading Information:", JOptionPane.INFORMATION_MESSAGE);
+	public String tradingInfo = "     Trading played a significant role in the Oregon Trail experience."
 			+ "\nThis activity was essential for obtaining important supplies like"
-			+ "\nfood, securing services such as transportation, and receiving"
-			+ "\nassistance along the journey. While trading could take place anywhere,"
-			+ "\nit was especially common at trading posts or forts located along the"
-			+ "\ntrail. The large amount of trading that occurred helped to develop and"
+			+ " food, \nsecuring services such as transportation, and receiving"
+			+ " assistance \nalong the journey. While trading could take place anywhere,"
+			+ " it was \nespecially common at trading posts or forts located along the"
+			+ "trail. \nThe large amount of trading that occurred helped to develop and"
 			+ "\nstrengthen the local economy in the region.";
 	/**
 	 * Trading played a significant role in the Oregon Trail experience.
@@ -108,7 +118,8 @@ public class LandmarkDescriptions {
 	 * strengthen the local economy in the region.
 	 */
 	
-	public String illnessInfo = "The two main reasons people died on the Oregon Trail were serious"
+	// JOptionPane.showMessageDialog(null, illnessInfo, "Illness Information:", JOptionPane.INFORMATION_MESSAGE);
+	public String illnessInfo = "     The two main reasons people died on the Oregon Trail were serious"
 			+ "\ndiseases and unfortunate accidents. Among these diseases, cholera"
 			+ "\nwas the most deadly, affecting about ten percent of the emigrants"
 			+ "\nwho undertook the journey. Cholera is a bacterial infection that"
@@ -133,14 +144,16 @@ public class LandmarkDescriptions {
 	 * someone got sick.
 	 */
 	
-	public String beginningInfo = "Welcome to Perils Along the Platte! Did you know that the Platte"
+	// JOptionPane.showMessageDialog(null, beginningInfo, "Beginning Introduction:", JOptionPane.INFORMATION_MESSAGE);
+	public String beginningInfo = "     Welcome to Perils Along the Platte! Did you know that the Platte"
 			+ "\nRiver played a crucial role in the journey of pioneers along the"
 			+ "\niconic Oregon Trail? Spanning 310 miles of the trail's total of"
 			+ "\nover 2000 miles, this picturesque river valley was a favorite among"
 			+ "\nthose traveling westward. With its gently flowing waters and fertile"
 			+ "\ngrazing lands, it offered a perfect path for wagons and provided"
 			+ "\nessential resources for livestock. The ambitious pioneers relied on"
-			+ "\nthe Oregon Trail as their gateway to new opportunities and land out west.";
+			+ "\nthe Oregon Trail as their gateway to new opportunities and land"
+			+ "\nout west.";
 	/**
 	 * Welcome to Perils Along the Platte! Did you know that the Platte
 	 * River played a crucial role in the journey of pioneers along the
@@ -152,7 +165,8 @@ public class LandmarkDescriptions {
 	 * the Oregon Trail as their gateway to new opportunities and land out west. 
 	 */
 	
-	public String endingInfo = "Congratulations on successfully navigating the Platte River! It's"
+	// JOptionPane.showMessageDialog(null, endingInfo, "The End:", JOptionPane.INFORMATION_MESSAGE);
+	public String endingInfo = "     Congratulations on successfully navigating the Platte River! It's"
 			+ "\nnoteworthy that only around 90% of travelers managed to get past"
 			+ "\nthis point on the Oregon Trail. The estimated mortality rate along"
 			+ "\nthe trail ranged from 4% to 10%, with disease and accidents being"
